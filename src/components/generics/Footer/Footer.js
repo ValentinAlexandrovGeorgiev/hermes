72e29e1648c5ccac139a30175a58a03071c7eff1
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2017-10-30 00:49:41 +0200

    add footer styling add header styling

--- src/components/generics/Footer/Footer.js
+++ src/components/generics/Footer/Footer.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './footer.scss'
 
+const FB_STYLE = {
+  border: 'none',
+  overflow: 'hidden'
+}
 class Footer extends Component {
   render () {
     return (
       <div className='footer__wrapper'>
-        <div className='footer__item'>
-          <Link to='/products'>Продукти</Link>
+        <div className='col col-xs-100 col-md-33 footer__column'>
+          <div className='footer__item'>
+            <Link to='/products'>Продукти</Link>
+          </div>
+          <div className='footer__item'>
+            <Link to='/services'>Печатница</Link>
+          </div>
         </div>
-        <div className='footer__item'>
-          <Link to='/services'>Печатница</Link>
+        <div className='col col-xs-100 col-md-33 footer__column'>
+          <div className='footer__item'>
+            <Link to='/catalogs'>Каталози</Link>
+          </div>
+          <div className='footer__item'>
+            <Link to='/for-us'>За нас</Link>
+          </div>
         </div>
-        <div className='footer__item'>
-          <Link to='/catalogs'>Каталози</Link>
-        </div>
-        <div className='footer__item'>
-          <Link to='/for-us'>За нас</Link>
-        </div>
-        <div className='footer__facebook_wrapper'>
-
+        <div className='col col-xs-100 col-md-33 footer__fb_plugin'>
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fhermesgift&width=340&height=250&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1723121821304727" width="340" height="250" style={FB_STYLE} scrolling="no" frameBorder="0" allowTransparency="true"></iframe>
         </div>
       </div>
     )
