d172bccc766045485169844dc3339607f2f334ee
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2017-10-31 22:40:06 +0200

    add statistics animation

--- src/components/home/Statistics/Statistics.js
+++ src/components/home/Statistics/Statistics.js
@@ -9,19 +9,31 @@ class Statistics extends Component {
       <div className='statistics__wrapper'>
         <div className='statistics__item col col-xs-100 col-md-33'>
           <Link className='statistics__link' to='/products'>
-            <span className='statistics__number'>{'4000'}</span>
+            <div className='statistics__number'>
+              <div className='statistics__outer'>
+                {'4000'}
+              </div>
+            </div>
             <p className='statistics__label'>{'Продукти на склад'}</p>
           </Link>
         </div>
         <div className='statistics__item col col-xs-100 col-md-33'>
           <Link className='statistics__link' to='/catalogs'>
-            <span className='statistics__number'>{'50 000'}</span>
+            <div className='statistics__number'>
+              <div className='statistics__outer'>
+                {'50 000'}
+              </div>
+            </div>
             <p className='statistics__label'>{'Продукти, внос от Европа'}</p>
           </Link>
         </div>
         <div className='statistics__item col col-xs-100 col-md-33'>
           <Link className='statistics__link' to='/services'>
-            <span className='statistics__number'>{'17'}</span>
+            <div className='statistics__number'>
+              <div className='statistics__outer'>
+                {'17'}
+              </div>
+            </div>
             <p className='statistics__label'>{'Печатни услуги'}</p>
           </Link>
         </div>
