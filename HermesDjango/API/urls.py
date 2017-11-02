c214fbace8551554a6159c7358e4015d6107f6f1
Ivelin <benitezzz@abv.bg>
2017-11-02 03:47:58 +0200

    extending API

--- HermesDjango/API/urls.py
+++ HermesDjango/API/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from rest_framework import renderers
 from API.views import AssetViewSet, CatalogViewSet, CategoryViewSet,\
-    ProdcutViewSet, SiteViewSet
+    ProdcutViewSet, SiteViewSet, ProductByCategory, SiteConfig
 
 
 category_list = CategoryViewSet.as_view({
@@ -46,8 +46,12 @@ asset_detail = AssetViewSet.as_view({
 
 urlpatterns = ([
     url(r'^product/(?P<pk>[0-9]+)/$', product_detail, name='product-detail'),
+    url(r'^products/(?P<category_name>[\w\-]+)/$', ProductByCategory.as_view(),
+        name='products-category'),
     url(r'^categories/$', category_list, name='category-list'),
     url(r'^catalogs/$', catalog_list, name='catalog-list'),
     url(r'^asset/(?P<pk>[0-9]+)/$', asset_detail, name='asset_detail'),
+    url(r'^assets/(?P<config_name>[\w\-]+)/$', SiteConfig.as_view(),
+        name='site-config')
 
 ])
--- HermesDjango/API/views.py
+++ HermesDjango/API/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from API.models import Asset, Catalog, Category, Product, Site
 from API.serializers import AssetSerializer, CatalogSerializer,\
     CategorySerializer, ProductSerializer, SiteSerializer
@@ -28,3 +31,30 @@ class SiteViewSet(viewsets.ReadOnlyModelViewSet):
 class AssetViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Asset.objects.all()
     serializer_class = AssetSerializer
+
+
+class ProductByCategory(APIView):
+
+    def get_category(self, name):
+        try:
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_name):
+        category = self.get_category(category_name)
+        category_products = Product.objects.filter(category=category)
+        if category_products:
+            serialized_products = ProductSerializer(category_products)
+            return Response(serialized_products.data)
+        raise Http404
+
+
+class SiteConfig(APIView):
+
+    def get(self, request, config_name):
+        site = Site.objects.get(pk=1)
+        serialized_assets = AssetSerializer(site.asset_set.all())
+        if serialized_assets:
+            return Response(serialized_assets.data)
+        raise Http404
