d1dbb182f652b85b078954232471a83a70d82db6
Ivelin <benitezzz@abv.bg>
2017-11-08 23:43:18 +0200

    Allow spaces in category names for API request

--- HermesDjango/API/urls.py
+++ HermesDjango/API/urls.py
@@ -46,12 +46,12 @@ asset_detail = AssetViewSet.as_view({
 
 urlpatterns = ([
     url(r'^product/(?P<pk>[0-9]+)/$', product_detail, name='product-detail'),
-    url(r'^products/(?P<category_name>[\w\-]+)/$', ProductByCategory.as_view(),
+    url(r'^products/(?P<category_name>[\w\- ]+)/$', ProductByCategory.as_view(),
         name='products-category'),
     url(r'^categories/$', category_list, name='category-list'),
     url(r'^catalogs/$', catalog_list, name='catalog-list'),
     url(r'^asset/(?P<pk>[0-9]+)/$', asset_detail, name='asset_detail'),
-    url(r'^assets/(?P<config_name>[\w\-]+)/$', SiteConfig.as_view(),
+    url(r'^assets/(?P<config_name>[\w\- ]+)/$', SiteConfig.as_view(),
         name='site-config')
 
 ])
