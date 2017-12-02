66e4587e85ca58867d51ee36e1682c4df96734d4
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2017-12-02 23:42:56 +0200

    Add npm commands move the server under 8081

--- front/sources/links.js
+++ front/sources/links.js
@@ -1,4 +1,4 @@
-const SERVER_HTTP_URL = 'https://localhost:8080'
+const SERVER_HTTP_URL = 'https://localhost:8081'
 
 export const GET_PRODUCT_BY_ID = `${SERVER_HTTP_URL}/api/product/`
 export const GET_CATEGORY_PRODUCTS = `${SERVER_HTTP_URL}/api/products/`
