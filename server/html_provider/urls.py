21c1045fe1e0a69f157618a4c99a6b1cedb40db4
Ivelin <benitezzz@abv.bg>
2017-12-05 00:44:45 +0200

    Fixing routing conflict

--- server/html_provider/urls.py
+++ server/html_provider/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import serve_base_html, serve_service_worker
 
 urlpatterns = [
-    url(r'^$', serve_base_html, name='serve-base-html'),
     url(r'^service-worker/$', serve_service_worker,
         name='serve-service-worker'),
+    url(r'^(?:.*)/?$', serve_base_html, name='serve-base-html')
 ]
